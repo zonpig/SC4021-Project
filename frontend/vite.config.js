18b858887565c93788cd58c575eac6b7a27f63e6
etjr2001 <erictanjr21@gmail.com>
2025-04-12 20:56:56 +0800

    Resolve config issues

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from "vite";
+import  { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -27,6 +28,22 @@ export default defineConfig({
           });
         },
       },
+    },
+  },
+  optimizeDeps: {
+    include: ["clsx", "tiny-invariant", "fast-equals", "prop-types"],
+  },
+  resolve: {
+    alias: {
+      "clsx": path.resolve(__dirname, "node_modules/clsx"),
+      "fast-equals": path.resolve(__dirname, "node_modules/fast-equals"),
+      "tiny-invariant": path.resolve(__dirname, "node_modules/tiny-invariant"),
+      'prop-types': path.resolve(__dirname, 'node_modules/prop-types')
+    },
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/], // Include necessary node_modules for CommonJS support
     },
   },
 });
